75bd0ab56257a934f4a2a5d808d9e55179548fe3
hiperpowe <139306439+hiperpowe@users.noreply.github.com>
2026-03-01 13:13:56 -0700

    Add files via upload

---
+++ Main Loadstrings.lua
@@ -0,0 +1,11 @@
+cooolchill_x Main HUB:
+loadstring(game:HttpGet("https://raw.githubusercontent.com/cooolchillX/cooolchillX-HUB/main/HUB.lua"))()
+
+cooolchill_x CMD Hub:
+loadstring(game:HttpGet("https://raw.githubusercontent.com/cooolchillX/cooolchillX-HUB/main/CMD%20Hub.lua"))()
+
+Hypers HUB:
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hiperpowe/Roblox-Hypers-HUB/refs/heads/main/HypersHUB.lua"))()
+
+infiniteyield:
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
