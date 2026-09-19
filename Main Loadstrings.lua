90fb266807fc5a2f8b4f27ff5e6c9bf5f0c9ebe7
hiperpowe <139306439+hiperpowe@users.noreply.github.com>
2026-09-18 20:37:04 -0600

    Update Main Loadstrings.lua

--- Main Loadstrings.lua
+++ Main Loadstrings.lua
@@ -1,5 +1,5 @@
 cooolchill_x Main HUB:
-loadstring(game:HttpGet("https://raw.githubusercontent.com/cooolchillX/cooolchillX-HUB/main/HUB.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/cooolchillX/cooolchillX-HUB/refs/heads/main/Hub%20Loadstring.lua"))()
 
 cooolchill_x CMD Hub:
 loadstring(game:HttpGet("https://raw.githubusercontent.com/cooolchillX/cooolchillX-HUB/main/CMD%20Hub.lua"))()
